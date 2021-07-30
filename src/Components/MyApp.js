c5db479abcf2617ce35ea2d79ae24c6f55ca5161
Akshay Tarde <akshaymtarde@gmail.com>
2021-07-30 09:25:57 +0530

    Fixed the state of the checkboxes and made them dynamic.Completed the todo app.

--- src/Components/MyApp.js
+++ src/Components/MyApp.js
@@ -2,15 +2,45 @@ import React from "react";
 import TodoItem from "./TodoItem";
 import todosData from "../todosData.js";
 
-function MyApp() {
-  const todoComponent = todosData.map((item) => (
-    <TodoItem key={item.id} todosData={item} />
-  ));
-  return (
-    <div className="AppStyle">
-      <div className="card">{todoComponent}</div>
-    </div>
-  );
+class MyApp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(id) {
+    this.setState((prevState) => {
+      const updatedTodo = prevState.todos.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      });
+      return {
+        todos: updatedTodo,
+      };
+    });
+  }
+
+  render() {
+    const todoComponent = this.state.todos.map((item) => (
+      <TodoItem
+        handleChange={this.handleChange}
+        key={item.id}
+        todosData={item}
+      />
+    ));
+    return (
+      <div className="AppStyle">
+        <div className="card">{todoComponent}</div>
+      </div>
+    );
+  }
 }
 
 export default MyApp;
--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,14 +1,25 @@
 import React from "react";
 
 function TodoItem(props) {
+  const completedList = {
+    color: "#cdcdcd",
+    textDecoration: "line-through",
+    fontStyle: "italic",
+  };
   return (
     <div className="checkbox">
       <input
         type="checkbox"
         checked={props.todosData.completed}
         className="button"
+        onChange={() => props.handleChange(props.todosData.id)}
       />
-      <p className="text">{props.todosData.text}</p>
+      <p
+        className="text"
+        style={props.todosData.completed ? completedList : null}
+      >
+        {props.todosData.text}
+      </p>
     </div>
   );
 }
